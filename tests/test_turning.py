03011c4975ae50a0fb617f67330e34f9137fd9b2
Evan Klitzke <evan@yelp.com>
2008-07-12 12:53:42 -0700

    add a failing test case

--- tests/test_turning.py
+++ tests/test_turning.py
@@ -15,6 +15,9 @@ def make_vector(direction_in_degrees, x_pos, y_pos):
 def steer_to_origin(vec):
     return mars_math.steer_to_point(vec, 1, mars_math.Point(0.0, 0.0))
 
+def steer_to_point(vec, x, y):
+    return mars_math.steer_to_point(vec, 1, mars_math.Point(float(x), float(y)))
+
 class TestTurning(TestCase): 
     "Try parsing the sample message from the manual"
 
@@ -38,6 +41,13 @@ class TestTurning(TestCase):
         rover_vec = make_vector(270.0, -5, 0)
         turn_angle, t = steer_to_origin(rover_vec)
         assert_is_right_turn(turn_angle)
+
+        # rover is at (1, 1) heading to (2,2). Trying to navigate to (1, 0)
+        rover_vec = make_vector(45.0, 1, 1)
+        turn_angle, t = steer_to_point(rover_vec, 1, 0)
+        assert_is_right_turn(turn_angle)
+
+
 
 
 if __name__ == '__main__':

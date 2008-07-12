fe098280ec6a1908fb51cb47b11627ca8d3a9328
Evan Klitzke <evan@yelp.com>
2008-07-12 13:39:44 -0700

    more test cases

--- tests/test_turning.py
+++ tests/test_turning.py
@@ -24,7 +24,7 @@ def steer_to_point(vec, x, y):
 class TestTurning(TestCase): 
     "Try parsing the sample message from the manual"
 
-    def test(self):
+    def test_moving_to_origin_from_one_one(self):
         # rover is at (1, 1) heading straight up. should turn left
         rover_vec = make_vector(90.0, 1, 1)
         turn_angle, t = steer_to_origin(rover_vec)
@@ -35,6 +35,33 @@ class TestTurning(TestCase):
         turn_angle, t = steer_to_origin(rover_vec)
         assert_is_right_turn(turn_angle)
 
+    def test_moving_below_or_above(self):
+        # rover is at (1, 1) heading to (2,2). Trying to navigate to (1, 0).
+        # This should be a right turn
+        rover_vec = make_vector(45.0, 1, 1)
+        turn_angle, t = steer_to_point(rover_vec, 1, 0)
+        assert_is_right_turn(turn_angle)
+
+        # rover is at (1, 1) heading to (2,2). Trying to navigate to (1, 2).
+        # This should be a left turn
+        rover_vec = make_vector(45.0, 1, 1)
+        turn_angle, t = steer_to_point(rover_vec, 1, 2)
+        assert_is_left_turn(turn_angle)
+
+    def test_moving_normal(self):
+        # rover is at (1, 1) heading to (2,2). Trying to navigate to (0, 5).
+        # This should be a left turn
+        rover_vec = make_vector(45.0, 1, 1)
+        turn_angle, t = steer_to_point(rover_vec, 0, 5)
+        assert_is_left_turn(turn_angle)
+
+        # rover is at (1, 1) heading to (2,2). Trying to navigate to (0, -5).
+        # This should be a right turn
+        rover_vec = make_vector(45.0, 1, 1)
+        turn_angle, t = steer_to_point(rover_vec, 0, -5)
+        assert_is_right_turn(turn_angle)
+
+    def test_boundary_conditions(self):
         # rover is at (5, 0) heading straight up. should turn left
         rover_vec = make_vector(90.0, 5, 0)
         turn_angle, t = steer_to_origin(rover_vec)
@@ -54,18 +81,6 @@ class TestTurning(TestCase):
         rover_vec = make_vector(270.0, -5, 0)
         turn_angle, t = steer_to_origin(rover_vec)
         assert_is_right_turn(turn_angle)
-
-        # rover is at (1, 1) heading to (2,2). Trying to navigate to (1, 0).
-        # This should be a right turn
-        rover_vec = make_vector(45.0, 1, 1)
-        turn_angle, t = steer_to_point(rover_vec, 1, 0)
-        assert_is_right_turn(turn_angle)
-
-        # rover is at (1, 1) heading to (2,2). Trying to navigate to (1, 2).
-        # This should be a left turn
-        rover_vec = make_vector(45.0, 1, 1)
-        turn_angle, t = steer_to_point(rover_vec, 1, 2)
-        assert_is_left_turn(turn_angle)
 
 if __name__ == '__main__':
     main() 

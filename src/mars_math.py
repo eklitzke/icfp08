af4b9810e7030ac296e6a5ffd795442a49556cd8
Evan Klitzke <evan@thinkpad.(none)>
2008-07-11 22:13:36 -0700

    some incompelte math

--- src/mars_math.py
+++ src/mars_math.py
@@ -14,6 +14,47 @@ class Ellipse(object):
 		self.min = min
 		self.max = max
 
+class Point(object):
+	def __init__(self, x, y):
+		self.x = float(x)
+		self.y = float(y)
+
+	def add(p):
+		return Point(self.x + p.x, self.y + p.y)
+
+	def norm(self):
+		return math.sqrt(self.x**2 + self.y**2)
+
+class Angle(object):
+	def __init__(object, radians):
+		self.radians = float(radians) / (2 * math.pi)
+		self.degrees = self.radians * 180 / math.pi
+	
+	def __add__(self, theta):
+		return Angle(self.radians + theta.radians)
+
+	def invert(self):
+		return Angle(-1 * self.radians)
+
+class Vector(object):
+	def __init__(self, s, angle, pos=None):
+		self.s = float(s)
+
+		self.angle = angle
+
+		# aliases
+		self.speed = self.s
+		self.angle = self.thea
+		
+		self.pos = pos if pos is not None else Point(0, 0)
+
+		self.vx = self.speed * math.cos(self.theta)
+		self.vy = self.speed * math.cos(self.theta)
+
+	def future_position(self, t):
+		vec = Point(self.vx * t, self.vy * t)
+		return self.pos.add(vec)
+
 # terminal velocity = sqrt ( a / k)
 #
 # dt should be the time for the first telemetry update, v should be the
@@ -24,3 +65,22 @@ def calculate_coefficients(dt, v, tv):
 	a = float(v) / (dt**2) # need to go back and redo this right with calculus
 	k = a / (tv**2)
 	return a, k
+
+# FIXME: this is for figuring out martian triangles
+def predicted_path(vec, omega, dt):
+	'''omega is the guess of the maximum angular velocity of the object over
+	the time interval, vec should be a Vector, dt is the time interval that
+	we're interested in measuring over.'''
+
+	straight_point = vec.future_position(dt)
+
+	# FIXME: this approximation only works if dt is small, but it can be
+	# exactly computed if i spend a little bit more work to figure out the
+	# right equation... This is an overestimate.
+
+	# omega should be radians / second
+	angle = Angle(abs(omega) * dt)
+	right_angle = vec.angle + angle
+	left_angle = vec.angle + angle.invert()
+
+	leg_distance = math.sin(angle.radians) * vec.speed * dt
